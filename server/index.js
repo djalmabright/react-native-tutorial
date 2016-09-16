bf8aa95950b6c98719ec592519534c91c6fe9bab
joehandlebar <suk.chang@rangle.io>
2016-09-16 12:17:36 -0400

    Resolve babel issue

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('babel-register')({presets: ['es2015']});
+
 const express = require('express');
 const passport = require('passport');
 const GithubStrategy = require('passport-github2').Strategy;
