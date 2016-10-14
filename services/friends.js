42f3c75808854fc5df5e216329c410dfbbbc25c5
Chris Bond <chris.bond@rangle.io>
2016-10-14 16:46:39 -0400

    Use template strings

--- services/friends.js
+++ services/friends.js
@@ -2,12 +2,12 @@ import {config} from '../constants';
 
 export class FriendsService {
   getUser(accessToken) {
-    return fetch(config.host + '/user?accessToken=' + accessToken)
+    return fetch(`${config.host}/user?accessToken=${accessToken}`)
       .then(res => res.json());
   }
 
   getFriends(accessToken) {
-    return fetch(config.host + '/friends?accessToken=' + accessToken)
+    return fetch(`${config.host}/friends?accessToken=${accessToken}`)
       .then(res => res.json());
   }
 }
