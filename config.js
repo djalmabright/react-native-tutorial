ebff6ed9c8f4927caccf769327104cd8b13361b3
Djalma  Bina <djbina2000@yahoo.com.br>
2017-11-05 00:00:26 -0200

    Update config.js

--- config.js
+++ config.js
@@ -6,17 +6,17 @@ export const config = {
   },
   pubnub: {
     authKey: 'server-auth',
-    subscribeKey: 'sub-c-25ff9f44-7f85-11e6-8a0d-0619f8945a4f',
-    publishKey: 'pub-c-52fbe46d-e262-4034-91ae-c9495b7550e6',
-    secretKey: 'sec-c-ZjZkMzJiYzgtMTdhNC00MmZjLWIxNDEtMDVlNTkxZTQyOTkz',
+    subscribeKey: 'sub-c-6867f9ec-c1cb-11e7-b683-b67c7dbcdd0',
+    publishKey: 'pub-c-665f0ca6-59c7-4022-b9e7-d1648fe8c76f'
+    secretKey: 'sec-c-ZjZkMzJiYzgtMTdhNC00MmZjLWIxNDEtMDVlNTkxZTQyOTkz' 
   },
   github: {
     android: {
-      clientId: '22ef2f2ea7fc5cf1d125',
+      clientId: '22ef2f2ea7fc5cf1d1
       clientSecret: 'c965f686a901a338a81d6a50f81aad750705a328',
     },
     ios: {
-      clientId: '4591e867f77e815d446e',
+      clientId: '4591e867f77e815d446e'
       clientSecret: 'a96497bed46dd07786b9b2fff16d59e25c9758cc',
     },
   },
